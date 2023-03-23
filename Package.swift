38daa504bac8457c03f46d2bb19f5bb555e25dc8
pawan-100ms <pawan@100ms.live>
2023-03-23 16:08:03 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,25 +8,21 @@ let package = Package(
     products: [
         .library(
             name: "HMSSDK",
-            targets: ["HMSSDK"])
+            targets: ["HMSSDK", "WebRTC"])
     ],
     dependencies: [
-        .binaryTarget(
-            name: "WebRTC",
-            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5115/WebRTC.xcframework.zip",
-            checksum: "4596d1383d45d206cdced61237f6fc5cef4793f749f19e1fe7e4af64d2db42c2"
-        ),
-        .binaryTarget(
-            name: "HMSAnalyticsSDK",
-            url: "https://github.com/100mslive/100ms-ios-analytics-sdk/releases/download/0.0.2/HMSAnalyticsSDK.xcframework.zip",
-            checksum: "40229908576cac8afab7f9ba8b3bd9b1408f97f7bff63f83dca5b4f60f4378f0"
-        )
+        .package(url: "https://github.com/100mslive/100ms-ios-analytics-sdk", from: "0.0.2")
     ],
     targets: [
         .binaryTarget(
             name: "HMSSDK",
             url: "https://github.com/100mslive/100ms-ios-sdk/releases/download/0.7.1/HMSSDK.xcframework.zip",
             checksum: "434cbf07c6340ece46fd5b49d6e3b16b21060b72b60de61b613068e4d2c5d0c6"
+        ),
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5115/WebRTC.xcframework.zip",
+            checksum: "4596d1383d45d206cdced61237f6fc5cef4793f749f19e1fe7e4af64d2db42c2"
         )
     ]
 )
