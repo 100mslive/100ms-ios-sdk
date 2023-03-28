8adc4e031d7d1567a80494c703483bc90e4862be
pawan-100ms <pawan@100ms.live>
2023-03-28 14:49:45 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "HMSSDK",
-            targets: ["HMSSDK", "WebRTC", "dependencies"])
+            targets: ["HMSSDK", "WebRTC", "HMSSDKDependencies"])
     ],
     dependencies: [
         .package(name: "HMSAnalyticsSDK", url: "https://github.com/100mslive/100ms-ios-analytics-sdk", from: "0.0.2"),
@@ -24,6 +24,6 @@ let package = Package(
             url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5115/WebRTC.xcframework.zip",
             checksum: "4596d1383d45d206cdced61237f6fc5cef4793f749f19e1fe7e4af64d2db42c2"
         ),
-        .target(name: "dependencies", dependencies: ["HMSAnalyticsSDK"], path: "dependencies")
+        .target(name: "HMSSDKDependencies", dependencies: ["HMSAnalyticsSDK"], path: "dependencies")
     ]
 )
