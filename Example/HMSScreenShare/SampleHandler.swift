521f17d249284db2089ca26997cc0ed695e06c25
pawan-100ms <pawan@100ms.live>
2023-09-18 21:57:07 +0400

    Update example apps to use 1.0.0 Broadcast extension

--- Example/HMSScreenShare/SampleHandler.swift
+++ Example/HMSScreenShare/SampleHandler.swift
@@ -6,55 +6,14 @@
 //  Copyright © 2022 100ms. All rights reserved.
 //
 
-import ReplayKit
 import HMSBroadcastExtensionSDK
 
-class SampleHandler: RPBroadcastSampleHandler {
-
-    let screenRenderer = HMSScreenRenderer(appGroup: "group.live.100ms.videoapp")
-    
-    override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
-        // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional.
+class SampleHandler: HMSBroadcastSampleHandler {
+    override var appGroupId: String {
+        "group.live.100ms.videoapp"
     }
     
-    override func broadcastPaused() {
-        // User has requested to pause the broadcast. Samples will stop being delivered.
-    }
-    
-    override func broadcastResumed() {
-        // User has requested to resume the broadcast. Samples delivery will resume.
-    }
-    
-    override func broadcastFinished() {
-        // User has requested to finish the broadcast.
-        self.screenRenderer.invalidate()
-    }
-    
-    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
-        switch sampleBufferType {
-        case RPSampleBufferType.video:
-            // Handle video sample buffer
-            if let error = screenRenderer.process(sampleBuffer) {
-                if error.code == .noActiveMeeting {
-                    finishBroadcastWithError(NSError(domain: "ScreenShare", code: error.code.rawValue, userInfo: [NSLocalizedFailureReasonErrorKey : "You are not in a meeting."]))
-                }
-            }
-        case RPSampleBufferType.audioApp:
-            // Handle audio sample buffer for app audio
-            
-            if let error = self.screenRenderer.process(audioSampleBuffer: sampleBuffer) {
-                if error.code == .noActiveMeeting {
-                    finishBroadcastWithError(NSError(domain: "ScreenShare", code: error.code.rawValue, userInfo: [NSLocalizedFailureReasonErrorKey : "You are not in a meeting."]))
-                }
-            }
-            
-            break
-        case RPSampleBufferType.audioMic:
-            // Handle audio sample buffer for mic audio
-            break
-        @unknown default:
-            // Handle other sample buffer types
-            fatalError("Unknown type of sample buffer")
-        }
+    override var enableAudio: Bool {
+        true
     }
 }
