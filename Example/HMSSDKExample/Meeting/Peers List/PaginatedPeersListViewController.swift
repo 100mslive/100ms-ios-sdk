843c71d6b2297b83d4e9ee8d21269171813983d7
gzerad <dfedoseyev@gmail.com>
2023-09-28 15:21:29 +0300

    1.1.0 Release

---
+++ Example/HMSSDKExample/Meeting/Peers List/PaginatedPeersListViewController.swift
@@ -0,0 +1,215 @@
+//
+//  PeersListViewController.swift
+//  HMSSDKExample
+//
+//  Created by Yogesh Singh on 28/02/21.
+//  Copyright © 2021 100ms. All rights reserved.
+//
+
+import UIKit
+import HMSSDK
+
+final class PaginatedPeersListViewController: UIViewController {
+
+    @IBOutlet private weak var participantsTitle: UIButton!
+
+    @IBOutlet private  weak var table: UITableView!
+    @IBOutlet private  weak var loadMoreButton: UIButton!
+
+    internal var roomName: String!
+
+    internal var meetingViewModel: MeetingViewModel?
+
+    internal var speakers: [HMSViewModel]?
+
+    typealias DataSource = UITableViewDiffableDataSource<PeersSection, HMSPeer>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<PeersSection, HMSPeer>
+
+    private lazy var dataSource = makeDataSource()
+
+    private var peers = [HMSPeer]()
+    private var iterator: HMSPeerListIterator?
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        observeParticipants()
+
+        applySnapshot()
+
+        iterator = meetingViewModel?.interactor?.hmsSDK?.getPeerListIterator(options: HMSPeerListIteratorOptions(limit: 10))
+        loadPeers()
+    }
+    
+    func loadPeers() {
+        iterator?.next(completion: { [weak self] loadedPeers, error in
+            guard let self = self else { return }
+            if let loadedPeers = loadedPeers {
+                self.peers.append(contentsOf: loadedPeers)
+                self.applySnapshot()
+                self.loadMoreButton.isHidden = !(iterator?.hasNext ?? false)
+            }
+        })
+    }
+
+    fileprivate func updatePeersCount() {
+        let count = meetingViewModel?.interactor?.hmsSDK?.room?.peerCount ?? 0
+        let title = "Peers " + (count > 0 ? "(\(count))" : "")
+        participantsTitle.setTitle(title, for: .normal)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        updatePeersCount()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    // MARK: - View Modifiers
+
+    func makeDataSource() -> DataSource? {
+
+        let dataSource = DataSource(tableView: table) { (table, indexPath, peer) -> UITableViewCell? in
+
+            guard let cell = table.dequeueReusableCell(withIdentifier: "Cell",
+                                                       for: indexPath) as? PeersListTableViewCell else {
+                return nil
+            }
+
+            self.update(cell, for: peer)
+
+            return cell
+        }
+        return dataSource
+    }
+
+    func update(_ cell: PeersListTableViewCell, for peer: HMSPeer) {
+        cell.peer = peer
+        cell.nameLabel.text = peer.name
+        if let role = peer.role?.name {
+            cell.roleLabel.text = role.capitalized
+        }
+        cell.handRaiseButton.isHidden = !peer.isHandRaised
+
+        if #available(iOS 14.0, *) {
+            if let menu = meetingViewModel?.getMenu(for: peer) {
+                cell.settingsButton.menu = menu
+                cell.settingsButton.showsMenuAsPrimaryAction = true
+                cell.settingsButton.isEnabled = true
+            } else {
+                cell.settingsButton.isEnabled = false
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+
+        updatePeersCount()
+
+        updateSpeaker(cell, peer)
+    }
+
+    func updateSpeaker(_ cell: PeersListTableViewCell, _ peer: HMSPeer) {
+
+        if speakers?.first(where: { $0.peer.peerID == peer.peerID }) != nil {
+            let animatedImage = UIImage.animatedImage(with: animatedImages(), duration: 1)
+            cell.speakingImageView.image = animatedImage
+        } else {
+            cell.speakingImageView.image = UIImage(systemName: "speaker.wave.2.fill")
+        }
+    }
+
+    func applySnapshot(animatingDifferences: Bool = true) {
+
+        guard let dataSource = dataSource else { return }
+
+        var snapshot = Snapshot()
+
+        snapshot.appendSections([.main])
+
+        snapshot.appendItems(peers)
+
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+
+    // MARK: - Action Handlers
+
+    private func observeParticipants() {
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateList(with:)),
+                                               name: Constants.joinedRoom,
+                                               object: nil)
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateList(with:)),
+                                               name: Constants.peersUpdated,
+                                               object: nil)
+
+        _ = NotificationCenter.default.addObserver(forName: Constants.updatedSpeaker,
+                                                   object: nil,
+                                                   queue: .main) { [weak self] notification in
+
+            guard let userInfo = notification.userInfo,
+                  let speakers = userInfo["speakers"] as? [HMSViewModel]
+            else { return }
+
+            self?.speakers = speakers
+
+            for speaker in speakers {
+                if let index = self?.dataSource?.indexPath(for: speaker.peer),
+                   let cell = self?.table.cellForRow(at: index) as? PeersListTableViewCell {
+                    self?.updateSpeaker(cell, speaker.peer)
+                }
+            }
+
+            if speakers.count == 0 {
+                self?.table.reloadData()
+            }
+        }
+    }
+
+    @objc private func updateList(with notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let peer = userInfo["peer"] as? HMSPeer,
+              let dataSource = dataSource else { return }
+
+        if let index = dataSource.indexPath(for: peer),
+           let cell = table.cellForRow(at: index) as? PeersListTableViewCell {
+
+            update(cell, for: peer)
+        }
+
+        applySnapshot()
+    }
+
+    @IBAction private func closeTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction private func loadMoreTapped(_ sender: UIButton) {
+        loadPeers()
+    }
+}
+
+extension PaginatedPeersListViewController {
+
+    func animatedImages() -> [UIImage] {
+
+        var images = [UIImage]()
+
+        for counter in 1...3 {
+            if let regular = UIImage(systemName: "speaker.wave.\(counter)")?.withTintColor(.link),
+               let filled = UIImage(systemName: "speaker.wave.\(counter).fill")?.withTintColor(.link) {
+                images.append(regular)
+                images.append(filled)
+            }
+        }
+
+        return images
+    }
+}
--- HMSSDK.podspec
+++ HMSSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'HMSSDK'
-  s.version          = '1.0.1'
+  s.version          = '1.1.0'
   s.summary          = 'HMS Videoconferencing iOS SDK'
 
   s.description      = <<-DESC
@@ -10,8 +10,8 @@ TODO: Add long description of the pod here.
   s.homepage         = 'https://github.com/100mslive/100ms-ios-sdk/'
   s.license          = { :type => 'MIT'}
   s.author           = { 'Dmitry Fedoseyev' => 'dmitry@100ms.live', 'Yogesh Singh' => 'yogesh@100ms.live', 'Pawan Dixit' => 'pawan@100ms.live'}
-  s.source           = { :http => 'https://github.com/100mslive/100ms-ios-sdk/releases/download/1.0.1/HMSSDK.xcframework.zip',
-                           :sha256 => '448cca359ab0ed127bc4c00681f16217cc10c37c51d5096609002ec9d992f0bb'
+  s.source           = { :http => 'https://github.com/100mslive/100ms-ios-sdk/releases/download/1.1.0/HMSSDK.xcframework.zip',
+                           :sha256 => '637c962cef051b4e78d458dff03752a0e5a659bd96f93f3bc6252823b9e7cbb7'
 						}
   s.ios.deployment_target = '12.0'
   s.vendored_frameworks = 'HMSSDK.xcframework'
